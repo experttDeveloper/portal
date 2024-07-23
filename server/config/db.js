89dec15bdc1e8a133bbfa97f148eeafce5356b31
Expert developer <experttdeveloper@gmail.com>
2024-07-23 11:12:13 +0530

    test

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // MongoDB connection URL
-const databaseUrl = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/deep_cafe';
+const databaseUrl = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/portal';
 
 // Connect to MongoDB
 mongoose.connect(databaseUrl, {
